aa02408fcf1a26b69970d002191dd0f653273b32
Manoj Ghimire <manoj.ghimire7@gmail.com>
2020-09-29 15:03:39 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '329302734832372',
+        clientSecret: '7d328c47cd506ade78e0a303e09aad5e'
+    }
 }
